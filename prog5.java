1edc85907ebcfb0ea18164bd2cad7c7f3e10f26c
java_code_practices <ravipandey8832@gmail.com>
2024-09-13 00:04:08 +0530

    java_string_reverse

---
+++ prog5.java
@@ -0,0 +1,22 @@
+//reverse the string in java
+package javaLocal;
+import java.util.*;
+public class prog5 {
+    public static void main(String arg[]){
+        StringBuilder sb  = new StringBuilder("Hello");
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front = i;
+            int back = sb.length()-1-i;
+
+           char frontchar = sb.charAt(front);
+           char backchar = sb.charAt(back);
+
+       
+           sb.setCharAt(front, backchar);
+           sb.setCharAt(back, frontchar);
+        }
+        System.out.println(sb);
+    }
+    
+}
