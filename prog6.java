dc6ba1813c0c4fe9c56637da748ff336c741f778
java_code_practices <ravipandey8832@gmail.com>
2024-09-13 23:41:41 +0530

    prog6

---
+++ prog6.java
@@ -0,0 +1,26 @@
+package javaLocal;
+
+import java.util.*;
+
+public class prog6 {
+    public static void main(String arg[]) {
+        // pre increment
+        int a = 10;
+        int b = 0;
+        b = a++;
+        System.out.println(a);
+        System.out.println(b);
+    
+        System.out.println();
+
+       // post increment
+       int x= 10;
+       int y = 0;
+
+       y= ++x;
+    System.out.println(x);
+    System.out.println(y);
+ 
+    
+}
+}
