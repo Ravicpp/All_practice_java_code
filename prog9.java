6436edcf4335d22f609350507ca8c7d6e47249c3
java_code_practices <ravipandey8832@gmail.com>
2024-09-16 22:38:00 +0530

    prog9

---
+++ prog9.java
@@ -0,0 +1,50 @@
+//check prime number
+// package javaLocal;
+// import java.util.*;
+// public class prog9 {
+//     public static void main(String arg[]){
+    //   Scanner sc  = new Scanner(System.in);
+    //   System.out.println("Enter the number");
+    //   int num = sc.nextInt();
+    //   int flag = 0;
+    //   for(int i=2;i<num/2;i++){
+    //        if(num%i==0){
+    //         System.out.println("not prime number");
+    //         flag = 1;
+    //        }
+    //   }
+    //   if(flag ==0){
+    //     System.out.println("Prime number");
+
+    //   }
+
+// check even odd
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter your number");
+    // int n = sc.nextInt();
+
+    // if(n%2==0){
+    //     System.out.println("This is even");
+    // }else{
+    //     System.out.println("This is odd");
+    // }
+
+// print factorial in n number
+
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter your number");
+//     int n = sc.nextInt();
+//     int sum =0;
+//    int fact = 1;
+//     for(int i=1;i<=n;i++){
+       
+//         fact = fact*i;
+//         sum = sum+fact;
+//     }
+//     System.out.println("the factorial is "+ fact);
+
+//     }
+// }
+
+
