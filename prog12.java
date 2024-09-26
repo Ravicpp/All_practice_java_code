69879df5c64bf8ce35fa206da1e629abd5d49242
java_code_practices <ravipandey8832@gmail.com>
2024-09-26 21:41:56 +0530

    prog12Java

---
+++ prog12.java
@@ -0,0 +1,26 @@
+package javaLocal;
+
+import java.util.*;
+
+public class prog12 {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String arg[]) {
+        int[] arr = { 4, 1, 3, 5, 7 };
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        printArray(arr);
+    }
+}
